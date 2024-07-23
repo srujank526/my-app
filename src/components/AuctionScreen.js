4483f37b05509bfcc37eeea561bb192ab18ba210
srujank526 <srujank526@gmail.com>
2024-07-24 00:27:13 +0530

    commit

--- src/components/AuctionScreen.js
+++ src/components/AuctionScreen.js
@@ -2,31 +2,39 @@ import './AuctionScreen.css'
 import React,{useState} from 'react'
 
 
-function AuctionScreen({socket}){
+function AuctionScreen({socket,isGameEnd,handleGameEnd}){
 
     const [currSet,setCurrSet] = useState('')
+    const [showSetButton,setshowSetButton] = useState(true)
     const [currPlayer,setCurrPlayer] = useState('')
 
     function getSet(){
+        setshowSetButton(false)
         socket.emit('reqSet')
         socket.on('resSet',(data)=>{
             setCurrSet(data)
+            if(data === null){
+                handleGameEnd(true)
+            }
         })
     }
     function getPlayer(){
         socket.emit('reqPlayer')
         socket.on('resPlayer',(data)=>{
             setCurrPlayer(data)
+            if(data === null)setshowSetButton(true)
         })
     }
     
-    return (
+    return (<>
+        {isGameEnd?<h1>End of Auction....<br/> Thank you for playing</h1>:
         <div className='screen'>
-            Current Set: {currSet}
-            {currPlayer?`Current Player: ${currPlayer.name}`:''}
-            {currPlayer?'':<button onClick={getSet}>get Set</button>}
-            <button onClick={getPlayer} disabled={!currSet}>get player</button>
-        </div>
+            <div>Current Set: {showSetButton?'Draw a Set...':currSet}</div>
+            {currPlayer?`Current Player: ${currPlayer.name}`:''}<br/>
+            {showSetButton?<button onClick={getSet}>get Set</button>:<button onClick={getPlayer} disabled={!currSet}>get player</button>}
+            
+        </div>}
+        </>
     );
 }
 export default AuctionScreen;
--- src/components/AuctionTable.js
+++ src/components/AuctionTable.js
@@ -1,15 +1,19 @@
+import React,{useState} from "react";
 import AuctionScreen from "./AuctionScreen";
 import './AuctionTable.css'
 function AuctionTable({ socket,users }) {
-    console.log(users)
+    const [isGameEnd,setIsGameEnd] = useState(false);
+    function handleGameEnd(isGameEnd){
+        setIsGameEnd(isGameEnd)
+    }
     return (
         <>
-            <AuctionScreen socket={socket} />
+            <AuctionScreen socket={socket} isGameEnd={isGameEnd} handleGameEnd={handleGameEnd}/>
             <div className="usersFloor">
                 {users.map((user) => (
                     <div key={user.socket}>
                         {user.name}
-                        {socket.id === user.socket?<button>BID</button>:''}
+                        {(socket.id === user.socket && !isGameEnd)?<button>BID</button>:''}
                     </div>
                 ))}
             </div>
